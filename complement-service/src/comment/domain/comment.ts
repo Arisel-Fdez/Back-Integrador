c83bcf092a4d5b1d6a4cf2c2f17918545535e307
Arisel Fernandez Cañaveral <84791183+Arisel-Fdez@users.noreply.github.com>
2023-12-05 20:58:45 -0600

    Delete complement-service directory

--- complement-service/src/comment/domain/comment.ts
+++
@@ -1,9 +0,0 @@
-export class Comment {
-    constructor(
-        readonly id: number,
-        readonly userId: number,
-        readonly publicationId: number,
-        readonly content: string,
-        readonly userName?: string
-    ) {}
-}
