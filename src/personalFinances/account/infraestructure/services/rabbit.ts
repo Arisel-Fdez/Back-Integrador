db4a96fe8c47f6d06fd156edc3468363cbcc1283
Arisel-Fdez <211119@ids.upchiapas.edu.mx>
2023-12-05 23:28:50 -0600

    FINAL

--- src/personalFinances/account/infraestructure/services/rabbit.ts
+++ src/personalFinances/account/infraestructure/services/rabbit.ts
@@ -6,7 +6,7 @@ export class RabbitMQ implements RabbitMQService {
     private connection: amqp.Connection | null = null;
 
     async connect(): Promise<void> {
-        this.connection = await amqp.connect('amqps://b-9d29a2d4-604e-4ebd-92c1-39952aad2d85.mq.us-east-1.amazonaws.com:5671');
+        this.connection = await amqp.connect('amqp://b-9d29a2d4-604e-4ebd-92c1-39952aad2d85.mq.us-east-1.amazonaws.com:5671');
     }
 
     async publishMessage(exchange: string, routingKey: string, message: any): Promise<void> {
